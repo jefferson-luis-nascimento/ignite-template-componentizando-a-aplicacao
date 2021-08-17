7be5528f382d98bd5ca3bf1db0480258a7c02632
Jefferson Luís Nascimento <je.luis.nascimento@hotmail.com>
2021-08-17 11:26:10 -0300

    implemented issues

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
+import { useState } from "react";
+import { Button } from "./Button";
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  selectedGenreId: number;
+  genres: Genre[];
+  handleClickButton(id:number): void;
+}
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {props.genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => props.handleClickButton(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+  );
 }
